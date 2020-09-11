daa1b01f27e4c07fdf925e857b400a3c08f51541
C0d3m4j1c <salvador_jimenez@outlook.es>
2020-09-10 22:37:12 -0500

    Added seeder

--- AT.DataAccess/Data/ATDbContext.cs
+++ AT.DataAccess/Data/ATDbContext.cs
@@ -28,5 +28,14 @@ namespace AT.DataAccess.Data
         public virtual DbSet<User> Users{get;set;}
         public virtual DbSet<Product> Products{get;set;}
         public virtual DbSet<ProductType> ProductTypes{get;set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            #region ProductType
+            modelBuilder.Entity<ProductType>().HasData(new ProductType {Id = 1, ProductTypeName = "Type1"});
+            modelBuilder.Entity<ProductType>().HasData(new ProductType {Id = 2, ProductTypeName = "Type2"});
+            modelBuilder.Entity<ProductType>().HasData(new ProductType {Id = 3, ProductTypeName = "Type3"});
+            #endregion
+        }
     }
 }
